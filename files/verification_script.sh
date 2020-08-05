afc47568fd356d775bf1c72969461bd4b60f1cd9
root <root@server1.example.com>
2020-08-05 17:53:45 +0300

    Initial commit for web-role

---
+++ files/verification_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+  
+echo -e "The package installed       : $(rpm -qa | grep httpd-2 )"
+echo -e "HTTPD Service active state  : $(systemctl is-active httpd)"
+echo -e "HTTPD Service enable state  : $(systemctl is-enabled httpd)"
+echo -e "Firewall ports opened       : $(firewall-cmd --list-ports)"
+echo -e "Port Selinux                : $(semanage port -l | grep ^http_port_t)"
+echo -e "Root directory Selinux      : \n$(ls -aZ /vhosts/ | grep 'httpd_sys_content_t')"
+echo -e "Port in configuration       : $(grep ^Listen /etc/httpd/conf.d/$1-vhost.conf)"
+echo -e ""
+echo -e "Configuration file          : \n$(cat /etc/httpd/conf.d/$1-vhost.conf)"
+echo -e ""
+echo -e "Home page                   : \n$(curl -s localhost:$2)"
+echo -e ""
+exit 0
